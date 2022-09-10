e76ce635053da8b1471df60e3d08acd09e9a14b6
Antony Yu <antonyyu@gmail.com>
2022-09-10 13:22:30 -0700

    Created Question Class and placeholder questions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-console.log("linked")
+// Question Class
+class Question {
+    constructor(question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3){
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.wrongAnswer1 = wrongAnswer1;
+        this.wrongAnswer2 = wrongAnswer2;
+        this.wrongAnswer3 = wrongAnswer3;
+    }
+}
+
+// Default questions
+
+let question1 = new Question(
+    "Question 1?",
+    "CorrectAnswer",
+    "Wrong Answer 1",
+    "Wrong Answer 2",
+    "Wrong Answer 3"
+)
+let question2 = new Question(
+    "Question 2?",
+    "CorrectAnswer",
+    "Wrong Answer 1",
+    "Wrong Answer 2",
+    "Wrong Answer 3"
+)
+let question3 = new Question(
+    "Question 3?",
+    "CorrectAnswer",
+    "Wrong Answer 1",
+    "Wrong Answer 2",
+    "Wrong Answer 3"
+)
+let question4 = new Question(
+    "Question 4?",
+    "CorrectAnswer",
+    "Wrong Answer 1",
+    "Wrong Answer 2",
+    "Wrong Answer 3"
+)
+
