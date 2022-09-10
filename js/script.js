09ef16ed10cbd3a65cdb9e2e2bbeb9c6201fa365
Antony Yu <antonyyu@gmail.com>
2022-09-10 15:57:19 -0700

    Question traversing functionality working

--- js/script.js
+++ js/script.js
@@ -13,31 +13,31 @@ class Question {
 
 let question1 = new Question(
     "Question 1?",
-    "CorrectAnswer",
-    "Wrong Answer 1",
-    "Wrong Answer 2",
-    "Wrong Answer 3"
+    "Q1CorrectAnswer",
+    "Q1Wrong Answer 1",
+    "Q1Wrong Answer 2",
+    "Q1Wrong Answer 3"
 )
 let question2 = new Question(
     "Question 2?",
-    "CorrectAnswer",
-    "Wrong Answer 1",
-    "Wrong Answer 2",
-    "Wrong Answer 3"
+    "Q2CorrectAnswer",
+    "Q2Wrong Answer 1",
+    "Q2Wrong Answer 2",
+    "Q2Wrong Answer 3"
 )
 let question3 = new Question(
     "Question 3?",
-    "CorrectAnswer",
-    "Wrong Answer 1",
-    "Wrong Answer 2",
-    "Wrong Answer 3"
+    "Q3CorrectAnswer",
+    "Q3Wrong Answer 1",
+    "Q3Wrong Answer 2",
+    "Q3Wrong Answer 3"
 )
 let question4 = new Question(
     "Question 4?",
-    "CorrectAnswer",
-    "Wrong Answer 1",
-    "Wrong Answer 2",
-    "Wrong Answer 3"
+    "Q4CorrectAnswer",
+    "Q4Wrong Answer 1",
+    "Q4Wrong Answer 2",
+    "Q4Wrong Answer 3"
 )
 
 let questionArray = [
@@ -62,43 +62,53 @@ document.getElementById('wrongAnswer1').addEventListener('click', wrongAnswerCho
 document.getElementById('wrongAnswer2').addEventListener('click', wrongAnswerChosen);
 document.getElementById('wrongAnswer3').addEventListener('click', wrongAnswerChosen);
 
+// For testing
+generateQuestion()
+
 function openNavDirections(){
     console.log("openNavDirections")
-
+    document.getElementById('directionsModal').style.display = 'block'
 }
 function pauseGame(){
     console.log("pauseGame")
+    document.getElementById('pauseModal').style.display = 'block'
 
 }
 function restartGame(){
     console.log("restartGame")
-
+    window.location.reload()
 }
 function closeGameStartModal(){
     console.log("closeGameStartModal")
-
+    document.getElementById('gameStartModal').style.display = 'none'
 }
 function closeDirectionsModal(){
     console.log("closeDirectionsModal")
-
+    document.getElementById('directionsModal').style.display = 'none'
 }
 function closePauseModal(){
     console.log("closePauseModal")
-
+    document.getElementById('pauseModal').style.display = 'none'
 }
 function correctAnswerChosen(){
     console.log("correctAnswerChosen")
-
+    document.getElementById('correctAnswerModal').style.display = 'block'
 }
 function wrongAnswerChosen(){
     console.log("wrongAnswerChosen")
-
+    document.getElementById('wrongAnswerModal').style.display = 'block'
 }
 
 function generateQuestion() {
-    document.getElementById('question').textContent = questionArray[0].question;
-    document.getElementById('correctAnswer').textContent = questionArray[0].correctAnswer;
-    document.getElementById('wrongAnswer1').textContent = questionArray[0].wrongAnswer1;
-    document.getElementById('wrongAnswer2').textContent = questionArray[0].wrongAnswer2;
-    document.getElementById('wrongAnswer3').textContent = questionArray[0].wrongAnswer3;
+    let randomQuestionArrayIndex = Math.floor(Math.random()*questionArray.length)
+    document.getElementById('question').textContent = questionArray[randomQuestionArrayIndex].question;
+    document.getElementById('correctAnswer').textContent = questionArray[randomQuestionArrayIndex].correctAnswer;
+    document.getElementById('wrongAnswer1').textContent = questionArray[randomQuestionArrayIndex].wrongAnswer1;
+    document.getElementById('wrongAnswer2').textContent = questionArray[randomQuestionArrayIndex].wrongAnswer2;
+    document.getElementById('wrongAnswer3').textContent = questionArray[randomQuestionArrayIndex].wrongAnswer3;
+    questionArray.splice(randomQuestionArrayIndex,1)
+    // Removes Correct!/Wrong! modal
+    document.getElementById('correctAnswerModal').style.display = 'none'
+    document.getElementById('wrongAnswerModal').style.display = 'none'
+
 }
